5d6091abff7c118c2b2d27effba2c30bfc694210
DrasLorus <draslorus@draslorus.fr>
2021-07-27 17:25:00 +0200

    Add copyright notice

--- shell/xilinx_report_parser.sh
+++ shell/xilinx_report_parser.sh
@@ -1,5 +1,23 @@
 #! /bin/bash
 
+#
+# Copyright© 2021 Camille 'DrasLorus' Monière
+# <draslorus@draslorus.fr> 
+#
+# This program is free software: you can redistribute it and/or modify
+# it under the terms of the GNU General Public License as published by
+# the Free Software Foundation, either version 3 of the License, or
+# (at your option) any later version.
+#
+# This program is distributed in the hope that it will be useful,
+# but WITHOUT ANY WARRANTY; without even the implied warranty of
+# MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+# GNU General Public License for more details.
+#
+# You should have received a copy of the GNU General Public License
+# along with this program.  If not, see <https://www.gnu.org/licenses/>.
+#
+
 TEMP=$(getopt --shell bash -o 'p:t:o:as' --long 'project-path:,report-type:,output:,all,separated' -n 'xilinx_parser.bash' -- "$@")
 
 if [ "$?" -ne 0 ]; then
